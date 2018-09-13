ba1e917c2c6c81576f94556dea05bd3d18f6b063
Juan Hernandez <jarceluz@gmail.com>
2018-09-13 00:05:26 -0700

    En esta aplicación se encuentra el primer ejercicio de la construccion de un TDA. usando elementos EOA

---
+++ Ejercicio-1/Ej_01/Ej_01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Library_Circle;
+namespace Ej_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Área Visual solo para el usuario se pueden implementar solo un metodo, o 
+            // Los metodos necesarios para su implementación, por ejemplo puede ser solo perimetro, o area , o diametro
+            // Segun lo que Pueda o requiera utilizar el desarrollador.
+
+            Console.WriteLine("Introduzca un numero de radio o finalice el programa con -1");
+            string radio = Console.ReadLine();
+            while (radio!="-1")
+            {
+                Circle circle = new Circle();
+                try
+                {
+                    decimal r = 0;
+                    bool isNumeric = decimal.TryParse(radio, out r);
+                    if (isNumeric == true)
+                    {
+                        circle.Mostrar(r);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Introduzca un numero valido, solo se aceptan caracteres numericos \n");
+                    }
+
+                    
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Sus datos no corresponden a un numero");
+                }
+
+                Console.WriteLine("Introduzca un numero de radio o finalice el programa con -1");
+                 radio = Console.ReadLine();
+            }
+
+            Console.WriteLine("#####  Gracias por Usar Nuestra App  #####");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ejercicio-1/Ej_01/Library_Circle/Circle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library_Circle
+{
+    public class Circle
+    {
+        // Un TDA debe tener datos privados como metodo de seguridad. y una parte publica
+        // que es lo que se encuentra en el main.
+
+        private decimal radio;
+        private const decimal PI=3.14159M;
+
+        //Los modificadores de accesos nos permiten tener el nivel necesario 
+        //de Operacion de un atributo.
+        //lo que seria nuestro letra E de element.
+        public decimal Radio
+        {
+            get
+            {
+                return this.radio;
+            }
+            set
+            {
+                this.radio = value;
+            }
+        }
+
+        // AREA de OPERACIONES SIMPLES
+        public void Mostrar(decimal radio)
+        {
+            Console.WriteLine("Sus Resultados son:");
+            Console.WriteLine("\n Área:"+CalcularArea(radio));
+            Console.WriteLine("\n Diametro: "+CalcularDiametro(radio));
+            Console.WriteLine("\n Perimetro: "+CalcularPerimetro(radio) + "\n");
+            Console.WriteLine(" ####### Fin ###### \n");
+        }
+
+        public decimal CalcularArea( decimal radio)
+        {
+            return PI * (radio * radio);
+        }
+
+        public decimal CalcularPerimetro(decimal radio)
+        {
+            return 2*PI*radio;
+        }
+
+        public decimal CalcularDiametro(decimal radio)
+        {
+            return 2 * radio;
+        }
+
+    }
+}
